664b4aebd00f1192846ebd635be2848c3e34d900
NAMDONGWOO <safetycho27@naver.com>
2023-04-21 17:02:23 +0900

    0421 SQL sequence rownum

--- 0417.sql
+++ 0417.sql
@@ -3117,5 +3117,261 @@ INSERT INTO test1(pw) VALUES ('1abABVF1234!');
 
 
 
+----------------------------------------------------------------------------------
+----------------------------------
+--
+/*
+1.1 SEQUENCE 특징 
+1) 자동적으로 유일 번호를 생성합니다.
+2) 공유 가능한 객체 (테이블간에)
+3) 주로 기본 키 값을 생성하기 위해 사용됩니다.
+4) 어플리케이션 코드를 대체합니다. (로직을 만들 필요가 없다)
+5) 메모리에 CACHE 되면 SEQUENCE 값을 액세스 하는 효율성을 향상시킵니다.
+
+CREATE SEQUENCE sequence_name
+[INCREMENT BY n]
+[START WITH n]
+[{MAXVALUE n | NOMAXVALUE}]
+[{MINVALUE n | NOMINVALUE}]
+[{CYCLE | NOCYCLE}]
+[{CACHE | NOCACHE}];
+
+sequence_name SEQUENCE 의 이름입니다.
+
+INCREMENT BY n 정수 값인 n 으로 SEQUENCE 번호 사이의 간격을 지정.
+이 절이 생략되면 SEQUENCE 는 1 씩 증가.
+START WITH n 생성하기 위해 첫번째 SEQUENCE 를 지정.
+이 절이 생략되면 SEQUENCE 는 1 로 시작.
+MAXVALUE n SEQUENCE 를 생성할 수 있는 최대 값을 지정.
+NOMAXVALUE 오름차순용 10^27 최대값과 내림차순용-1 의 최소값을 지정.
+MINVALUE n 최소 SEQUENCE 값을 지정.
+NOMINVALUE 오름차순용 1 과 내림차순용-(10^26)의 최소값을 지정.
+CYCLE | NOCYCLE 최대 또는 최소값에 도달한 후에 계속 값을 생성할 지의 여부를
+지정. NOCYCLE 이 디폴트.
+CACHE | NOCACHE 얼마나 많은 값이 메모리에 오라클 서버가 미리 할당하고 유지
+하는가를 지정. 디폴트로 오라클 서버는 20 을 CACHE
+*/
 
 
+desc board;
+
+drop table board;
+
+create table board(
+    boardid number constraint pk_board_boardid primary key,
+    title nvarchar2(50)
+);
+
+select * from user_constraints where table_name='BOARD';
+
+--not null, unique, index(검색속도)
+
+insert into board(boardid, title) values(1,'처음글');
+insert into board(boardid, title) values(2,'두번째');
+
+rollback;
+select * from board;
+
+-- 처음 글을 쓰면 글번호가 1번 .. 그 다움글 순차적인 증가값 2번,3번 ,....
+select count(boardid) +1 from board;
+
+insert into board(boardid, title) values ((select count(boardid) +1 from board) , '내용');
+
+insert into board(boardid, title) values ((select count(boardid) +1 from board) , '내용2');
+insert into board(boardid, title) values ((select count(boardid) +1 from board) , '내용3');
+
+select * from board;
+
+--게시글 삭제, 수정의 경우
+
+--게시글 삭제
+commit;
+
+delete from board where boardid=1;
+
+select * from board;
+
+commit;
+
+insert into board(boardid, title) values ((select count(boardid) +1 from board) , '새글'); -- 중복값이 발생
+
+--데이터가 삭제되어도 문제없는 순번
+
+insert into board(boardid, title) 
+values ((select nvl(max(boardid),0) +1 from board) , '새글');
+
+select * from board;
+
+--시퀀스 생성하기 (순번 만들기) : 객체(create ....) : 순차적인 번호를 생성하는  객체
+create sequence board_num;
+--순번
+select board_num.nextval from dual; --채번(번호표 뽑기)
+
+select board_num.currval from dual;
+
+--공유(객체) >> 하나의 테이블이 아니라 여러개의 테이블에서 사용
+
+--A테이블,                         B테이블 
+--(insert >>1)                    insert>>2, insert >>3
+--(insert >>4)
+
+create table kboard(
+    num number constraint pk_kboard_num primary key,
+    title nvarchar2(20)
+);
+
+create sequence kboard_num;
+
+insert into kboard(num, title) values (kboard_num.nextval,'처음');
+insert into kboard(num, title) values (kboard_num.nextval,'두번쨰');
+insert into kboard(num, title) values (kboard_num.nextval,'세번째');
+
+select * from kboard;
+
+--------------------------------------------------------------------------------
+/*
+-- 게시판
+-- 공지사항, 자유게시판, 답변형게시판 ....
+
+공지사항 1,2,3 (시퀀스객체1)
+
+자유게시판 1,2,3 (시퀀스객체2)
+
+답변형게시판 1,2,3 (시퀀스객체3) --최소 3개
+--------------------------------------------
+공지사항 1, 6
+
+자유게시판 2, 3
+
+답변형 게시판 4, 5
+
+시퀀스 객체 1개 - 3개의 테이블에서 사용 (공유객체)
+
+TIP)
+sequence 모든 DB에 ... 전부 있을까? (mysql, oracle , mariadb , mssql 등등)
+오라클 (o)
+ms-SQL (2012버전 (sequence))
+my-sql (x)
+maria db (o) --엔진(mysql)
+postgreSQL (o)
+
+--순번을 생성(테이블 종속으로)
+Ms-sql
+create table board(
+    boardnum int identity(1,1)....
+    title....
+)
+insert into board(title) values('제목'); >> boardnum >> 1이 자동
+
+my-sql
+create table board (boardnum int auto_increment, .... tile);
+
+insert into board(title) values('제목'); >> boardnum >> 1이 자동
+
+*/
+
+-- 옵션
+create sequence seq_num
+start with 10 
+increment by 2;
+
+select seq_num.nextval from dual;
+
+select seq_num.currval from dual;
+
+
+--순번 
+---게시판 에서 처음 , 데이터를 가져올 때
+-- 쿼리문
+--num > 1, 2, 3 ,,,,,, 10 ,,,,, 10000
+--가장 나중에 쓴 글 (최신글)
+--select * from board order by num desc;
+
+--rownum : 의사컬럼 : 실제 물리적으로 존재하는 컬럼이 아니고 논리적 존재 (
+-- create 할 때는 사용하지 않지만 select 할 때는 사용할 수 있는 column
+-- 실제로 테이블에 존재하지 않지만 내부적으로 행 번호를 부여하는 컬럼
+
+select * from emp;
+
+select rownum , empno , ename, sal from emp;
+
+
+select  empno , ename , sal
+from emp
+order by sal; 
+-- select가 order by 보다 먼저 실행되기 때문에 rownum의 번호가 아름답게 나오지 않는다.
+
+--급여를 많이 받는 순으로 정렬된 데이터 + rownum >> 순번을 가짐
+select rownum, e.empno, e.ename, e.sal
+from (
+        select  empno , ename , sal
+        from emp
+        order by sal desc --정렬의 기준 
+      ) e;
+
+--급여를 많이 받는 사원 5명
+
+select *
+from  (
+        select rownum as num, e.*
+        from (
+            select  empno , ename , sal
+            from emp
+            order by sal desc --정렬의 기준 
+        ) e
+       ) n 
+where num <= 5;
+--대용량 데이터에서 페이징 원리 (TODAY POINT)
+
+
+--between A and B -- 페이징 처리 가능
+----------------------------------------
+
+
+
+--rownum을 사용한 순서 원하는 select의 결과를 얻은후에 select에 붙임
+
+--TOP-n 쿼리 (기준이 되는 데이터 순으로 정렬 시키고 상위 n개를 가지고 오기)
+--MS-SQL : select top 10 , * from emp order by sal desc;
+
+--Oracle 에는 Top-n이 없다  / 대신 rownum 이있다.
+--rownum (순번부여 .... 상위 n)
+-- 1. 정렬의 기준 설정하기 (선행)
+-- 2. 정렬된 기준에 rownum 붙이고 ... 데이터 추출
+
+      
+-------------------------------------------------------------------------------------
+-- 기업 (10만건 ~ 1억건)
+-- 게시판 (게시글 10만건)
+--select * from board >> 10만건 조회 ..... >> 10건만 조회
+-- 10만건을 나누어서 확인 (10,20..등)
+
+/*
+totaldata = 100건
+pagesize =10 > 한 화면에 보여지는 데이터 row 수 : 10건
+
+page 개수 >> 10개 
+[1][2][3][4][5][6][7][8][9][10] 
+
+1page 클릭 >> 1~10번 까지 글 : DB 쿼리 : select num between 1 and 10
+<a href='page.jsp?page=1'></a>
+
+2page 클릭 >> 1~10번 까지 글 : DB 쿼리 : select num between 11 and 20
+
+방법 
+1. rownum
+2. between
+
+
+HR계정 이동
+
+*/
+
+show user;
+select * from employees; --107rjs
+
+-- 사원번호가 낮은 순으로 정렬하고 기준을 통해서 .... 41부터 50번쨰
+
+select  *
+from employees
+order by employe
